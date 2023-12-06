c575575eb9d27c8eb6a45f5ede11f280719eb04b
Nya Candy <dev@candinya.com>
2023-12-06 12:40:30 +0800

    fix: conf path

--- webpack.config.js
+++ webpack.config.js
@@ -739,7 +739,7 @@ module.exports = (env, argv) => {
                     { from: "media/**", context: path.resolve(__dirname, "node_modules/matrix-react-sdk/res/") },
                     "node_modules/@matrix-org/olm/olm_legacy.js",
                     { from: "config.json", noErrorOnMissing: true },
-                    { from: "conf.d/**", context: path.resolve(__dirname, "res") },
+                    { from: "conf.d/**", noErrorOnMissing: true },
                     "contribute.json",
                 ],
             }),
